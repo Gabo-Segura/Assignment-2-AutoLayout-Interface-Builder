3b6598fac47ba2b62c57dba63f6eca65e25da72c
Gabo-Segura <gabrielsegurao@me.com>
2021-12-13 23:06:54 -0500

    Finish assignment 2 Swift

---
+++ AutoLayout-Storyboard/AutoLayout-Storyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout-Storyboard
+//
+//  Created by Gabriel Eduardo on 12/13/21.
+//  Copyright © 2021 Gabriel Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
